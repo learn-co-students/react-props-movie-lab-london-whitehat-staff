8ffc114ef212fa51a4eddab768ee7647cfc94dab
Kelly <kelly.ho@sky.uk>
2019-03-25 16:09:30 +0000

    Done.

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -30,12 +30,9 @@ const posterMap = {
 export default class MovieCard extends Component {
 
   render() {
-
-    console.log(this.props)
     return (
       <div className="movie-card">
-        {/* which component should receive which props? */}
-        <CardFront />
+        <CardFront poster={posterMap[this.props.poster]}/>
         <CardBack 
         title={this.props.title} 
         IMDBRating={this.props.IMDBRating} 
